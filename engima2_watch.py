ffa710ae70fbaf9f9c2f8b7aa566a309561e4b87
Robbie <txt3rob@gmail.com>
2017-06-23 12:13:13 +0100

    Update engima2_watch.py

--- engima2_watch.py
+++ engima2_watch.py
@@ -22,6 +22,7 @@ def grab_playling (IP,PORT,CC):
 			URL = "http://"+IP+"/web/streamcurrent.m3u"
 		else:
 			URL = "http://"+IP+":"+PORT+"/web/streamcurrent.m3u"
+			
 		
 		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
 		response = session.get(URL, headers=headers, timeout=5)
